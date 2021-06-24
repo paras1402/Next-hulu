f8bc27568cd4e693ea37f47ace6fbf549c91216e
paras <paraskangra14@gmail.com>
2021-06-25 00:34:22 +0530

    added files

---
+++ components/HeaderItems.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeaderItems = ({ Icon, title }) => {
+  return (
+    <>
+      <div
+        className="flex flex-col items-center cursor-pointer group
+     w-12 sm:w-20 hover:text-white"
+      >
+        <Icon className="h-8 mb-1 hover:animate-bounce" />
+        <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+          {title}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default HeaderItems;
